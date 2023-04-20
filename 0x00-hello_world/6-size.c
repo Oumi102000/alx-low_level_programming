ccc82b89fc6d39ff17beae8ff3f16160f28c9ff1
Oumi102000 <o.elmadani10@gmail.com>
2023-04-20 23:32:37 +0000

    task 5 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main()
+{
+	    printf("with proper grammar, but the outcome is a piece of art,\n");
+	        printf("Size of a char: %d byte(s),\n");
+		printf("Size of an int: %d byte(s),\n");
+		printf("Size of a long int: %d byte(s),\n");
+		printf("Size of a long long int: %d byte(s),\n");
+		printf("Size of a float: %d byte(s),\n");
+
+		    return (0);
+}
