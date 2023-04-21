a9ec9d3688f4174fd12a815eb69d9bba7d52f7d0
Oumi102000 <o.elmadani10@gmail.com>
2023-04-21 00:04:49 +0000

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
-
-int  main(void)
+/**
+ * main - A programm with outpout size
+ * Return: 0 (Success)
+ */
+int main(void)
 {
-	    printf("with proper grammar, but the outcome is a piece of art,\n");
-	        printf("Size of a char: %d byte(s),\n");
-		printf("Size of an int: %d byte(s),\n");
-		printf("Size of a long int: %d byte(s),\n");
-		printf("Size of a long long int: %d byte(s),\n");
-		printf("Size of a float: %d byte(s),\n");
-
-		    return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("Size of a char: %d byte(s),\n");
+printf("Size of an int: %d byte(s),\n");
+printf("Size of a long int: %d byte(s),\n");
+printf("Size of a long long int: %d byte(s),\n");
+printf("Size of a float: %d byte(s),\n");
+return (0);
 }
