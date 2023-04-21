c312ccef0992c685c26f617dc3489aa398d30004
Oumi102000 <o.elmadani10@gmail.com>
2023-04-21 23:47:15 +0000

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char l;
+for (l = 'a'; l <= 'z'; l++)
+{
+if (l != 'e' && l != 'q')
+putchar(l);
+}
+putchar('\n');
+return (0);
+}
