bb0b70bb6899abba2e1df515f7d6b94a14c94c12
Oumi102000 <o.elmadani10@gmail.com>
2023-04-22 00:00:42 +0000

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *  main - Entry point
+ *   Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char l;
+for (l = 't'; l >= 'a'; l--)
+putchar(l);
+putchar('\n');
+return (0);
+}
