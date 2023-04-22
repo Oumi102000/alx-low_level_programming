cc8483042f07ecd773871d68852df05d0ae849d5
Oumi102000 <o.elmadani10@gmail.com>
2023-04-22 00:19:00 +0000

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,9 +12,9 @@ for (nb = 0; nb <= 9; nb++)
 {
 putchar((nb % 10) + '0');
 if (nb == 9)
-continue;  
+continue;
 putchar(',');
-putchar(' ');						        
+putchar(' ');
 }
 putchar('\n');
 return (0);
