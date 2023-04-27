30999e7b0edc01d212652e81384be3aa3ed5af45
Oumi102000 <o.elmadani10@gmail.com>
2023-04-27 00:28:13 +0000

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,9 +21,9 @@ _putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar(k + '0');
-} else if (k >= 10)
+}else if (k >= 10)
 {
-_putchar(','); 
+_putchar(',');
 _putchar(' ');
 _putchar((k / 10) + '0');
 _putchar((k % 10) + '0');
