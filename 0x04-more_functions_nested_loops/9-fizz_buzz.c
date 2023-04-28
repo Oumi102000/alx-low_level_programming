ec19c8ee2625466c1718fbe19670d81b390fc25e
Oumi102000 <o.elmadani10@gmail.com>
2023-04-28 01:04:43 +0000

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,7 +8,9 @@
  * Return: Always 0 (Success)
  */
 int main(void)
+
 {
+
 int i;
 
 for (i = 1; i <= 100; i++)
@@ -16,7 +18,7 @@ for (i = 1; i <= 100; i++)
 if (i % 3 == 0 && i % 5 != 0)
 {
 printf(" Fizz");
-} 
+}
 else if (i % 5 == 0 && i % 3 != 0)
 {
 printf(" Buzz");
@@ -24,11 +26,11 @@ printf(" Buzz");
 else if (i % 3 == 0 && i % 5 == 0)
 {
 printf(" FizzBuzz");
-} 
+}
 else if (i == 1)
 {
 printf("%d", i);
-} 
+}
 else
 {
 printf(" %d", i);
