e45d07b5609c57e13a2f842d11fdeede940a592e
Oumi102000 <o.elmadani10@gmail.com>
2023-05-06 00:37:25 +0000

    task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -3,15 +3,14 @@
 
 int main(void)
 {
-	    int n;
-	        int a[5];
-		    int *p;
-
-		        a[2] = 1024;
-			    p = (int*) malloc(sizeof(int));
-			        p = &n;
-				    *(p + 2) = 98; // modifies n and a[2]
-				        printf("a[2] = %d\n", a[2]); // should print 98
-					    free(p);
-					        return (0);
+ int n;
+ int a[5];
+ int *p;
+ a[2] = 1024;
+ p = (int*) malloc(sizeof(int));
+ p = &n;
+*(p + 2) = 98;
+printf("a[2] = %d\n", a[2]);
+free(p);
+return (0);
 }
